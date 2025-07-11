26e792eadff135323600eb57afdcfb2a6c27e229
Giovanny <gioguerra.je@gmail.com>
2025-07-11 22:25:39 +0200

    Practicaa Anidados

---
+++ practica00.py
@@ -0,0 +1,27 @@
+datos = {
+    "A001": ["Corolla", "Toyota", 2020, "Gasolina", "Sedan"],
+    "A002": ["Civic", "Honda", 2019, "Gasolina", "Sedan"],
+    "A003": ["Model 3", "Tesla", 2022, "Electrico", "Sedan"],
+    "A004": ["Rav4", "Toyota", 2021, "Hibrido", "SUV"],
+    "A005": ["CX-5", "Mazda", 2020, "Gasolina", "SUV"],
+    "A006": ["Mustang", "Ford", 2018, "Gasolina", "Coupe"],
+    "A007": ["Tucson", "Hyundai", 2021, "Gasolina", "SUV"],
+    "A008": ["Leaf", "Nissan", 2022, "Electrico", "Hatchback"],
+    "A009": ["Accord", "Honda", 2020, "Hibrido", "Sedan"],
+    "A010": ["Explorer", "Ford", 2019, "Gasolina", "SUV"]
+}
+inventario = {
+    "A001": [5, "Gasolina"],
+    "A002": [3, "Gasolina"],
+    "A003": [4, "Electrico"],
+    "A004": [6, "Hibrido"],
+    "A005": [0, "Gasolina"],
+    "A006": [2, "Gasolina"],
+    "A007": [7, "Gasolina"],
+    "A008": [3, "Electrico"],
+    "A009": [5, "Hibrido"],
+    "A010": [1, "Gasolina"]
+}
+
+while True:
+    
